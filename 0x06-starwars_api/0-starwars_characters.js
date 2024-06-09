3976455a501d196c3b831b08927ad015e28ef8fa
CodeX1991 <oghenewonaholy@gmail.com>
2024-06-09 06:48:38 +0000

    Using external api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-const process = require('process');
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching movie details:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Error fetching movie details: HTTP Status', response.statusCode);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const characterUrls = movieData.characters;
-
-  characterUrls.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character details:', charError);
-        return;
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Error fetching character details: HTTP Status', charResponse.statusCode);
-        return;
-      }
-
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
   });
-});
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
